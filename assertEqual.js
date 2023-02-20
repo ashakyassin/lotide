d4e3319c7ba91ba7185a95c808d6092ace7d7e2f
ashakyassin <120289196+ashakyassin@users.noreply.github.com>
2023-02-20 07:58:42 -0500

    Delete assertEqual.js

--- assertEqual.js
+++
@@ -1,9 +0,0 @@
-const assertEqual = function(actual, expected) {
-  // If actual is equal to expected then we would want to print assertion passed otherwise print assertion failed
-  if (actual === expected) {
-    console.log(`Assertion passed! ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion failed! ${actual} !== ${expected}`);
-  }
-};
-assertEqual("Lighthouse labs", "Bootcamp");
